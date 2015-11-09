157b1eb1a86d9b71021eb95abbe9d1af2bb857c3
LeonTG77 <leontg77@gmail.com>
2015-11-09 08:59:50 +0100

    Forgot to add the command register, oops

--- src/com/leontg77/eggs/Main.java
+++ src/com/leontg77/eggs/Main.java
@@ -3,6 +3,8 @@ package com.leontg77.eggs;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.leontg77.eggs.cmds.EggCommand;
+
 /**
  * Main class of the plugin.
  * 
@@ -27,6 +29,9 @@ public class Main extends JavaPlugin {
 		PluginDescriptionFile file = getDescription();
 		getLogger().info(file.getName() + " v" + file.getVersion() + " has been enabled.");
 		getLogger().info("Plugin is made by LeonTG77.");
+		
+		// register the /egg command.
+		getCommand("eggs").setExecutor(new EggCommand());
 		
 		// set the plugin field to this class.
 		plugin = this;
